80f592836b265a12da3e854fae71f4036e03472d
renatolins <renatolinsren@gmail.com>
2021-09-26 10:20:08 -0300

    create: verifyToken middleware

--- backend/routes/verifyToken.js
+++ backend/routes/verifyToken.js
@@ -1,15 +1,38 @@
 const jwt = require("jsonwebtoken");
 
 const verifyToken = (req, res, next) => {
-  res.send("verify token")
+  const authHeader = req.headers.token;
+
+  if (authHeader) {
+    const token = authHeader.split(" ")[1]
+    jwt.verify(token, process.env.JWT_SEC, (err, user) => {
+      if (err) res.status(403).json("Token is not valid!");
+      req.user = user;
+      next();
+    })
+  } else {
+    return res.status(401).json("You are not authenticated!");
+  }
 };
 
 const verifyTokenAndAuthorization = (req, res, next) => {
-  res.send("verify token and auth")
+  verifyToken(req, res, () => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("You are not alowed to do that!")
+    }
+  })
 };
 
 const verifyTokenAndAdmin = (req, res, next) => {
-  res.send("verify token and admin")
+  verifyToken(req, res, () => {
+    if (req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("You are not alowed to do that!")
+    }
+  })
 };
 
 module.exports = {
